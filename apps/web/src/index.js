3ffe05c0fecd6bd6eb3d5849cbd67ddbbe167098
Gokcen Ogutcu <gokcen@brewww.com>
2023-04-15 17:42:37 +0300

    feat: added dotenv, organized env var files among apps

--- apps/web/src/index.js
+++ apps/web/src/index.js
@@ -1,7 +1,10 @@
+import * as dotenv from 'dotenv';
 import React from "react";
 import PropTypes from 'prop-types';
 import { createRoot } from "react-dom/client";
 import { App } from "./App.js";
+
+dotenv.config();
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
