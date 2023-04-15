88794985bc000dccae198d7444753fc1deb3dde2
Gokcen Ogutcu <gokcen@brewww.com>
2023-04-15 17:41:57 +0300

    feat: added uppy

--- apps/web/src/App.js
+++ apps/web/src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import Uppy from '@uppy/core';
+import Webcam from '@uppy/webcam';
+import { Dashboard } from '@uppy/react';
+import XHR from '@uppy/xhr-upload';
+import * as path from 'path'
+
+import '@uppy/core/dist/style.min.css';
+import '@uppy/dashboard/dist/style.min.css';
+import '@uppy/webcam/dist/style.min.css';
+
+const uppy = new Uppy()
+  .use(Webcam)
+  .use(XHR, { 
+    endpoint: `${process.env.REACT_APP_API_BASEURL}/signin`,
+    fieldName: 'user_photo'
+  })
+  .on('complete', (file, response) => {
+    console.log(file.successful[0].response.body);
+});
 
 export function App() {
   return (
-    <div>Hello World!</div>
+    <Dashboard uppy={uppy} plugins={['Webcam']} />
   );
 }
