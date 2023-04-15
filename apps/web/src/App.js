ba85ad17aee17ccd0c31a626cf01efab55469785
Gokcen Ogutcu <gokcen@brewww.com>
2023-04-15 18:26:27 +0300

    fix: added uppy informer

--- apps/web/src/App.js
+++ apps/web/src/App.js
@@ -3,20 +3,25 @@ import Uppy from '@uppy/core';
 import Webcam from '@uppy/webcam';
 import { Dashboard } from '@uppy/react';
 import XHR from '@uppy/xhr-upload';
-import * as path from 'path'
+import Informer from '@uppy/informer';
 
 import '@uppy/core/dist/style.min.css';
 import '@uppy/dashboard/dist/style.min.css';
 import '@uppy/webcam/dist/style.min.css';
+import '@uppy/informer/dist/style.min.css';
 
-const uppy = new Uppy()
-  .use(Webcam)
+const uppy = new Uppy({ restrictions: {maxNumberOfFiles: 1}})
+  .use(Informer)
+  .use(Webcam, {
+    modes: ['picture'],
+    mirror: false
+  })
   .use(XHR, { 
     endpoint: `${process.env.REACT_APP_API_BASEURL}/signin`,
     fieldName: 'user_photo'
   })
   .on('complete', (file, response) => {
-    console.log(file.successful[0].response.body);
+    uppy.info(file.successful[0].response.body.name_surname);
 });
 
 export function App() {
