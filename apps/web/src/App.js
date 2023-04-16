1a5842d7ee71893263e6cc99468197d7cb9e3027
Gokcen Ogutcu <gokcen@brewww.com>
2023-04-16 09:54:39 +0300

    feat: implemented react-router

--- apps/web/src/App.js
+++ apps/web/src/App.js
@@ -4,6 +4,7 @@ import Webcam from '@uppy/webcam';
 import { Dashboard } from '@uppy/react';
 import XHR from '@uppy/xhr-upload';
 import Form from '@uppy/form';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 import '@uppy/core/dist/style.min.css';
 import '@uppy/dashboard/dist/style.min.css';
@@ -25,7 +26,7 @@ const uppy = new Uppy({ restrictions: {maxNumberOfFiles: 1}})
     file.successful.map(file => uppy.removeFile(file.id));
   });
 
-export function App() {
+function SignUp() {
   useEffect(() => {
       uppy.use(Form, {
       id: 'signup-form',
@@ -54,47 +55,69 @@ export function App() {
     }
   }
   return (
+    <div className="justify-center h-screen">
+      <form id="signup" className="bg-white shadow-md rounded px-4 pb-8 mb-2">
+        <div className="mb-4">
+          <Dashboard uppy={uppy} hideUploadButton={true} width={350} height={350} plugins={['Webcam']} />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name_surname">
+            Name Surname
+          </label>
+          <div className="relative">
+            <input
+              className="appearance-none block w-full border border-gray-400 rounded py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="name_surname"
+              name="name_surname"
+              type="text"
+              placeholder="Enter your PII"
+              value={inputValue}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            onClick={handleUpload}
+          >
+            Sign up 
+          </button>
+          &nbsp;&nbsp;
+          <Link className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" to="/sign-in">or Sign in</Link>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+function SignIn() {
+  return (
+    <div>
+      <h2>Sign In</h2>
+    </div>
+  );
+}
+
+function Layout() {
+  return (
     <div className="max-w-md mx-auto flex flex-col items-center">
-      <h1 className="text-3xl font-bold mb-6 pt-6">Sign up with TMRW ID</h1>
-      <div className="justify-center h-screen">
-        <form id="signup" className="bg-white shadow-md rounded px-4 pb-8 mb-2">
-          <div className="mb-4">
-            <Dashboard uppy={uppy} hideUploadButton={true} width={350} height={350} plugins={['Webcam']} />
-          </div>
-          <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name_surname">
-              Name Surname
-            </label>
-            <div className="relative">
-              <input
-                className="appearance-none block w-full border border-gray-400 rounded py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                id="name_surname"
-                name="name_surname"
-                type="text"
-                placeholder="Enter your PII"
-                value={inputValue}
-                onChange={handleInputChange}
-              />
-            </div>
-          </div>
-          <div className="flex items-center justify-center">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-              onClick={handleUpload}
-            >
-              Sign up 
-            </button>
-            &nbsp;&nbsp;
-             <a
-                className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                href="#"
-              >
-              or Sign in
-              </a>
-          </div>
-        </form>
-      </div>
+      <h1 className="text-3xl font-bold mb-6 pt-6">Proceed with TMRW ID</h1>
+      <Outlet />
+    </div>
+  );
+}
+
+export function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<SignUp />} />
+          <Route path="sign-in" element={<SignIn />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- apps/web/src/index.js
+++ apps/web/src/index.js
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv';
 import React from "react";
 import PropTypes from 'prop-types';
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { App } from "./App.js";
 
 import './assets/main.css';
@@ -12,5 +13,7 @@ const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-	<App/>
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
 );
