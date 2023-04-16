e0979bd7e8d5d309372b03159002c04cd23452d3
Gokcen Ogutcu <gokcen@brewww.com>
2023-04-16 17:14:07 +0300

    chore: try new webpack config for accessing env vars on render

--- apps/web/webpack.config.js
+++ apps/web/webpack.config.js
@@ -1,15 +1,9 @@
-const webpack = require('webpack');
-const dotenv = require('dotenv');
+const Dotenv = require('dotenv-webpack')
 
 module.exports = () => {
-  const env = dotenv.config().parsed;
-  const envKeys = Object.keys(env).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(env[next]);
-    return prev;
-  }, {});
   return {
     plugins: [
-      new webpack.DefinePlugin(envKeys)
+      new Dotenv()
     ]
   };
 };
